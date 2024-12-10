a7b880b21624e6e8394c2d25a2591756cf504c17
WHXFIGHTING <15245449+whx3638@user.noreply.gitee.com>
2024-12-10 18:18:45 +0800

    修改注释

--- iqtree.sh
+++ iqtree.sh
@@ -1,1 +1,3 @@
+#The construction of phylogenetic trees for various species in the database, with each species having its own fa file.
+
 iqtree -s all_new_mafft.fa -bb 1000 -bnni -alrt 1000 -bcor 0.99 -st AA -m LG+I+G4+F -nt 80
